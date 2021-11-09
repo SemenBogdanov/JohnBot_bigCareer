7d10d3c7029c657d68626494b08f91ddf1a0e155
Богданов Семен Павлович <Semen.Bogdanov@main.russianpost.ru>
2021-11-09 09:22:15 +0300

    four buttons added

--- johnbot.py
+++ johnbot.py
@@ -8,7 +8,8 @@ from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardMar
 from aiogram.utils.callback_data import CallbackData
 from key import token
 from aiogram import Bot, Dispatcher, executor, types
-from answers import answers
+from answers import answers, answers2
+import keyboards
 
 API_TOKEN = token
 logging.basicConfig(level=logging.INFO)
@@ -20,10 +21,13 @@ call_data2 = CallbackData('wantAsk', 'wantAsk')
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
     await message.reply("""Добрый день!\n"""
                         """Выбирайте самый сложный путь - там не конкурентов!\n\n"""
                         """Ниже есть кнопка, чтоб увидеть ответы на часто задаваемые вопросы. """,
-                        reply_markup=keyboard2())
+                        reply_markup=keyboards.keyboard2())
 
 
 def get_keyboard():
@@ -51,10 +55,23 @@ async def ask(message: types.Message):
     await message.reply('Выберите вопрос и нажмите кнопку:\n', reply_markup=get_keyboard())
 
 
-@dp.callback_query_handler(call_data1.filter(num=['1', '2', '3', '4', '5', '6', '7']))
+@dp.callback_query_handler(keyboards.call_data1.filter(num1=['1', '2', '3', '4', '5', '6', '7']))
 async def callback_reply(query: types.CallbackQuery, callback_data):
     await query.answer()
-    ans = answers[int(callback_data['num']) - 1]
+    if callback_data['num1'] == '2':
+        await bot.send_message(query.from_user.id, 'Выберите вариант:',
+                               reply_markup=keyboards.keyboard3())
+    else:
+        ans = answers[int(callback_data['num1']) - 1]
+        await bot.send_message(query.from_user.id, ans, parse_mode='html')
+    # logging.info('This what we have got %r', callback_data)
+    # logging.info('This is calldata1 %r', call_data1)
+
+
+@dp.callback_query_handler(keyboards.call_data3.filter(num2=['0', '1', '2', '3']))
+async def callback_reply(query: types.CallbackQuery, callback_data):
+    await query.answer()
+    ans = answers2[int(callback_data['num2'])]
     await bot.send_message(query.from_user.id, ans, parse_mode='html')
 
 
